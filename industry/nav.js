bad002e7efa2200b3e3ed450e26dfe3be7ddb740
iki1uc <entertrainer@web.de>
2026-08-27 22:36:41 +0200

    Update nav.js

--- industry/nav.js
+++ industry/nav.js
@@ -4,13 +4,18 @@ export function ORT_NAV() {
     const nav = document.getElementById("ort-nav");
 
     nav.innerHTML = `
-        <button data-go="home">Home</button>
-        <button data-go="use">Use</button>
-        <button data-go="user">User</button>
+        <button data-go="ORT">ORT</button>
+        <button data-go="TMP">TMP</button>
+        <button data-go="ACHSE">ACHSE</button>
+        <button data-go="LAGE">LAGE</button>
+        <button data-go="laTER">laTER</button>
+        <button data-go="RAUM">RAUM</button>
+        <button data-go="QUANT">QUANT</button>
+        <button data-go="SYN">SYN</button>
     `;
 
     nav.addEventListener("click", e => {
         const to = e.target.dataset.go;
-        if (to) ORT.navigate(to);
+        if (to) ORT.go(to);
     });
 }
