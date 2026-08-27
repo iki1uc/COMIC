5e377c67a6c060c143331516899f89ba5eb70eb3
iki1uc <entertrainer@web.de>
2026-08-27 20:14:16 +0200

    Create vis.js

---
+++ vis.js
@@ -0,0 +1,10 @@
+export function VIS_EXPORT(n6) {
+    return {
+        ware: n6.n1,
+        markt: n6.n2,
+        balance: n6.n3,
+        weg: n6.n4,
+        slide: n6.n5,
+        wette: n6.n6
+    };
+}
