1f393766294a25e128ae97f4c2030e3660b30f54
iki1uc <entertrainer@web.de>
2026-08-27 23:04:23 +0200

    Update masterboot.js

--- NC/masterboot.js
+++ NC/masterboot.js
@@ -1,25 +1,52 @@
 masterboot = {
 
+  // internes, sicheres Lesen (keine echte Netzverbindung)
   load: async function(file) {
-    return await OS.net.read(file)
-  },
 
-  apply: function(geo) {
-    OS.zone = geo.zone
-    OS.pos  = geo.pos
-    OS.dir  = geo.dir
-    OS.val  = geo.val
-    OS.mov  = geo.mov
-    OS.memory = geo.memory
-  },
-
-  start: async function() {
-
-    if (!OS.net.online()) {
-      throw "NET required: System cannot start offline."
+    // interne Simulation statt echter Netzwerk-Requests
+    if (!OS.net.files[file]) {
+      throw `BOOT ERROR: File '${file}' not found in tmp-storage.`;
     }
 
-    let geo = await this.load("BOOT.geo")
-    this.apply(geo)
+    return OS.net.files[file]; // rein intern
+  },
+
+  // Boot-Geometrie anwenden
+  apply: function(geo) {
+
+    // interne Schutzprüfung
+    if (!geo || typeof geo !== "object") {
+      throw "BOOT ERROR: Invalid BOOT.geo structure.";
+    }
+
+    OS.zone   = geo.zone   ?? "tmp-zone";
+    OS.pos    = geo.pos    ?? [0,0];
+    OS.dir    = geo.dir    ?? "none";
+    OS.val    = geo.val    ?? 0;
+    OS.mov    = geo.mov    ?? "idle";
+    OS.memory = geo.memory ?? {};
+  },
+
+  // vollständiger Startpfad
+  start: async function() {
+
+    // interne Online-Simulation
+    if (!OS.net.online()) {
+      throw "NET required: System cannot start offline.";
+    }
+
+    // BOOT.geo laden
+    let geo = await this.load("BOOT.geo");
+
+    // anwenden
+    this.apply(geo);
+
+    // interne Hardware-Simulation lesen
+    let hw = OS.sysview.read();
+
+    // interne Gefahrstufe berechnen
+    let danger = OS.sysview.danger(hw);
+
+    console.log("Gefahrstufe:", danger);
   }
-}
+};
