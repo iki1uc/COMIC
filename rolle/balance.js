3307e9145fb5c47664a900ae2fbb8b1a12901270
iki1uc <entertrainer@web.de>
2026-08-27 22:08:05 +0200

    Update balance.js

--- rolle/balance.js
+++ rolle/balance.js
@@ -1,3 +1,15 @@
+// BALANCE – COMIC 6-Linien-Achse
 export function BALANCE(l1, l2, l3, l4, l5, l6) {
-    return (l1 + l2 + l3 + l4 + l5 + l6) / 6;
+    const neutral = (l1 + l2 + l3 + l4 + l5 + l6) / 6;   // Neutralpunkt
+
+    const continuum = (l1 * l4) + (l2 * l5) + (l3 * l6); // Continuum-Achse
+
+    const sprung = continuum + neutral;                 // Achsen-Sprung
+
+    return {
+        neutral,
+        continuum,
+        sprung,
+        raster: [l1, l2, l3, l4, l5, l6]
+    };
 }
