64a23a0241d1eeee98b3f19daacff146be9e4cfe
iki1uc <entertrainer@web.de>
2026-08-27 23:55:07 +0200

    Update molecule.js

--- NC/L/molecule.js
+++ NC/L/molecule.js
@@ -6,7 +6,10 @@ export const Molecule = {
             name: "Wasser",
             atoms: ["H", "H", "O"],
             charge: Atom.charge([Atom.proton, Atom.proton, Atom.neutron]),
-            vital: Atom.vital(1, 1, 1)
+            vital: Atom.vital(1, 1, 1),
+            axis: 3,          // Wasser = Basisachse
+            layer: 1,         // unterste Schicht im 9³-Körper
+            index: 1          // Startpunkt
         };
     },
     O2() {
@@ -14,7 +17,10 @@ export const Molecule = {
             name: "Sauerstoff",
             atoms: ["O", "O"],
             charge: Atom.charge([Atom.neutron, Atom.neutron]),
-            vital: Atom.vital(2, 1, 1)
+            vital: Atom.vital(2, 1, 1),
+            axis: 6,          // Sauerstoff = Resonanzachse
+            layer: 2,         // mittlere Schicht
+            index: 2
         };
     },
     CO2() {
@@ -22,7 +28,10 @@ export const Molecule = {
             name: "Kohlenstoffdioxid",
             atoms: ["C", "O", "O"],
             charge: Atom.charge([Atom.neutron, Atom.neutron, Atom.neutron]),
-            vital: Atom.vital(3, 2, 1)
+            vital: Atom.vital(3, 2, 1),
+            axis: 9,          // CO2 = Impulsachse (ROLLE)
+            layer: 3,         // obere Schicht
+            index: 3
         };
     }
 };
