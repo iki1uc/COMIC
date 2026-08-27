cb83700224f6211376cb3b8b23c4343fc67b98cb
iki1uc <entertrainer@web.de>
2026-08-27 20:56:35 +0200

    Update 5.js

--- core/5.js
+++ core/5.js
@@ -1,1 +1,18 @@
+function forsakenGeometry(t) {
+  const base = 40 + Math.sin(t) * 10;
+
+  return {
+    triangle: [
+      { x: 0, y: -base },
+      { x: -base, y: base },
+      { x: base, y: base }
+    ],
+    pyramid: [
+      { x: 0, y: -base * 1.5 }, // Spitze
+      { x: -base, y: base },
+      { x: base, y: base },
+      { x: 0, y: base * 1.5 }  // Boden
+    ]
+  };
+}
 
