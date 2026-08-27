1fc4eba51ac25068740f1285710029b3f1600c90
iki1uc <entertrainer@web.de>
2026-08-27 22:08:48 +0200

    Update 4.js

--- rolle/4.js
+++ rolle/4.js
@@ -1,11 +1,26 @@
-// 4.js – Ausgang (Ergebnis)
+// AUSGANG – COMIC Ausgangs-Achse
 export function AUSGANG(wirkung) {
-    const ausgang = wirkung.wirkung / wirkung.eingang;
+    const eingang = wirkung.eingang;
+    const output = wirkung.wirkung;
+
+    // 1. Neutralpunkt
+    const neutral = (eingang + output) / 2;
+
+    // 2. Continuum-Achse
+    const continuum = eingang * output;
+
+    // 3. Sprung / Ausgang
+    const sprung = continuum + neutral;
+
+    // 4. Goldener Schnitt (6)
+    const gs6 = sprung / 1.618;
 
     return {
         ...wirkung,
-        ausgang,
-        gs6: ausgang // Goldener Schnitt (6)
+        neutral,
+        continuum,
+        sprung,
+        ausgang: sprung,
+        gs6
     };
 }
-
