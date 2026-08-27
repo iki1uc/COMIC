d242e20179d6277cb61066e5d61aa60ed3e74e2d
iki1uc <entertrainer@web.de>
2026-08-27 22:10:43 +0200

    Update 2.js

--- rolle/2.js
+++ rolle/2.js
@@ -1,11 +1,24 @@
-// 2.js – Station (Wert)
+// STATION – COMIC Wert-Achse
 export function STATION(eingang) {
-    const wert = eingang.preis * eingang.volumen * eingang.impuls;
+    const preis = eingang.preis;
+    const volumen = eingang.volumen;
+    const impuls = eingang.impuls;
+
+    // 1. Neutralpunkt
+    const neutral = (preis + volumen + impuls) / 3;
+
+    // 2. Continuum-Achse
+    const continuum = volumen * impuls;
+
+    // 3. Sprung / Wert
+    const sprung = continuum + preis;
 
     return {
         axis: "STATION",
         ...eingang,
-        wert
+        neutral,
+        continuum,
+        sprung,
+        wert: sprung
     };
 }
-
