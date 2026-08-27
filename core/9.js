ce5b36d1fb303385b1d1b02bbef38914346269d1
iki1uc <entertrainer@web.de>
2026-08-27 22:45:40 +0200

    Update 9.js

--- core/9.js
+++ core/9.js
@@ -1,45 +1,23 @@
-<script>
-let t = 0;
+// UNI-MATRIX – Natur/Atom/Gas/Industrie6 + Whirlwind-3-Potenzkette
 
-function animate() {
-  t += 0.02;
-  bx.clearRect(0,0,800,400);
+const UNI = {
+  POS: +1,              // positive Ladung
+  NEG: -1,              // negative Ladung
 
-  const F = forsakenBoost(t);
-  const O = n6Orbitals(t);
-  const B = run3Beam(t);
-  const G = ghost7Sinn(t);
-  const GEO = forsakenGeometry(t);
+  O2:  { orbit: O },    // Sauerstoff-Orbitale
+  CO2: { beam: B },     // CO2 lineare Struktur
 
-  // Beam
-  bx.strokeStyle = "#00ff88";
-  bx.beginPath();
-  bx.moveTo(B.pos, 200);
-  bx.lineTo(B.pos + 300, 200);
-  bx.stroke();
+  GAS: G,               // Ghost7Sinn → Gasverhalten / Energiezustand
+  GEO: GEO.pyramid,     // Molekül-Geometrie
 
-  bx.strokeStyle = "#ff4444";
-  bx.beginPath();
-  bx.moveTo(B.neg, 200);
-  bx.lineTo(B.neg + 300, 200);
-  bx.stroke();
-
-  // Atom Orbitale
-  drawOrbitals(bx, 600, 300, O);
-
-  // Forsaken Pyramide
-  bx.strokeStyle = "rgba(255,0,255,0.4)";
-  bx.beginPath();
-  GEO.pyramid.forEach((p, i) => {
-    const nx = 600 + p.x;
-    const ny = 300 + p.y;
-    if (i === 0) bx.moveTo(nx, ny);
-    else bx.lineTo(nx, ny);
-  });
-  bx.closePath();
-  bx.stroke();
-
-  requestAnimationFrame(animate);
-}
-animate();
-</script>
+  WHIRL: {
+    base: 3,            // Grundachse
+    seq: [3, 9, 27, 81, 243, 729], // 3^1 bis 3^6
+    pow: {
+      "3hoch8": Math.pow(3, 8),    // 6561
+      "3hoch81": Math.pow(3, 81)   // astronomisch groß
+    },
+    axiom: "3^n = Industrie-Dichte",
+    layer: "NC²-Dichte-Expansion"
+  }
+};
